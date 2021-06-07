04f55867c0792d3bf2512788ff18c2456e263322
st087823 <st087823@student.spbu.ru>
2021-06-08 00:14:54 +0300

    )))

--- Task2/Task2.cpp
+++ Task2/Task2.cpp
@@ -4,67 +4,36 @@
 
 using namespace std;
 
-bool isRussianVowel(char symbol)
-{
-	return symbol == 'À' || symbol == 'à' ||
-		symbol == 'Å' || symbol == 'å' ||
-		symbol == '¨' || symbol == '¸' ||
-		symbol == 'È' || symbol == 'è' ||
-		symbol == 'Î' || symbol == 'î' ||
-		symbol == 'Ó' || symbol == 'ó' ||
-		symbol == 'Û' || symbol == 'û' ||
-		symbol == 'Ý' || symbol == 'ý' ||
-		symbol == 'Þ' || symbol == 'þ' ||
-		symbol == 'ß' || symbol == 'ÿ';
-}
-bool isEnglishVowel(char symbol)
-{
-	return symbol == 'A' || symbol == 'a' ||
-		symbol == 'E' || symbol == 'e' ||
-		symbol == 'I' || symbol == 'i' ||
-		symbol == 'O' || symbol == 'o' ||
-		symbol == 'U' || symbol == 'u' ||
-		symbol == 'Y' || symbol == 'y';
-}
-
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	const int n = 32;
-	char a[n] = { "Êàê õîðîøî áûòü ïðîãðàììèñòîì" };
-	for (int i = 0; i < n; cout << a[i] << (i == n - 1 ? '\n' : ' '), ++i);
+	const int n = 16;
+	char a[16] = { "Íåèí ÷¸áë, ðòáëèí" };
 
 	fstream fbin;
+
 	fbin.open("in.bin", ios::binary | ios::out);
+	char d = 0;
+
 	if (!fbin)
 	{
-		cout << "Íå îòêðûëñÿ" << endl;
+		cout << "Íå îòêðûòü" << endl;
 	}
 	fbin.write(a, sizeof(a));
 	fbin.close();
 
-	char b = 0;
-
 	fbin.open("in.bin", ios::binary | ios::in | ios::out);
 	while (!fbin.eof())
 	{
-		fbin.read((char*)&b, sizeof(char));
-		cout << "read : " << b;
-		if (isRussianVowel(b))
+		fbin.read((char*)&d, sizeof(char));
+		cout << "read ";
+		cout << d << endl;
+		if (d == '¸' || d == 'û' || d == 'ý' || d == 'ó' || d == 'è' || d == 'î' || d == 'ÿ' || d == 'þ' || d == 'à' || d == 'å')
 		{
 			fbin.seekg(-(int)sizeof(char), ios::cur);
-			b = '#';
-			fbin.write((char*)&b, sizeof(char));
-			cout << " changed to #";
-			fbin.seekg(fbin.tellg(), ios::beg);
-		}
-		else if (isEnglishVowel(b))
-		{
-			fbin.seekg(-(int)sizeof(char), ios::cur);
-			b = '#';
-			fbin.write((char*)&b, sizeof(char));
-			cout << " changed to #";
+			d = '#';
+			fbin.write((char*)&d, sizeof(char));
 			fbin.seekg(fbin.tellg(), ios::beg);
 		}
 		cout << endl;
@@ -72,8 +41,12 @@ int main()
 	fbin.close();
 
 	fbin.open("in.bin", ios::in);
-	fbin.read((char*)&a, sizeof(a));
-	for (int i = 0; i < n; cout << a[i] << (i == n - 1 ? '\n' : ' '), ++i);
+	while (!fbin.eof())
+	{
+		fbin.read((char*)&d, sizeof(char));
+		cout << d;
+	}
+	cout << endl;
 	fbin.close();
 
 	return 0;
