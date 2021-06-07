86974f9ebcbbc36bbe8daddcdcfbaeb18a3d97df
st087823 <st087823@student.spbu.ru>
2021-06-08 01:55:25 +0300

    .

--- Task2/Task2.cpp
+++ Task2/Task2.cpp
@@ -1,53 +1,52 @@
 ﻿#include<iostream>
 #include<fstream>
-#include<clocale>
-
 using namespace std;
 
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	const int n = 16;
+	int n = 16;
 	char a[16] = { "Íåèí ÷¸áë, ðòáëèí" };
-
 	fstream fbin;
-
 	fbin.open("in.bin", ios::binary | ios::out);
-	char d = 0;
+	char d = '0';
 
 	if (!fbin)
 	{
-		cout << "Íå îòêðûòü" << endl;
+		cout << "не найден файл! " << endl;
 	}
 	fbin.write(a, sizeof(a));
 	fbin.close();
 
+	while (!fbin.eof())
+	{
+		fbin.read((char*)&d, sizeof(char));
+		cout << d;
+	}
+	fbin.close();
 	fbin.open("in.bin", ios::binary | ios::in | ios::out);
 	while (!fbin.eof())
 	{
 		fbin.read((char*)&d, sizeof(char));
-		cout << "read ";
+		cout << "считывай ";
 		cout << d << endl;
-		if (d == '¸' || d == 'û' || d == 'ý' || d == 'ó' || d == 'è' || d == 'î' || d == 'ÿ' || d == 'þ' || d == 'à' || d == 'å')
+		if (d == 'A' || d == 'E' || d == 'I' || d == 'O' || d == 'Y' || d == 'U' || d == 'a' || d == 'e' || d == 'i' || d == 'o' || d == 'u' || d == 'y')
 		{
 			fbin.seekg(-(int)sizeof(char), ios::cur);
 			d = '#';
 			fbin.write((char*)&d, sizeof(char));
 			fbin.seekg(fbin.tellg(), ios::beg);
 		}
-		cout << endl;
 	}
+	cout << endl;
 	fbin.close();
 
 	fbin.open("in.bin", ios::in);
-	while (!fbin.eof())
+	while (fbin.read((char*)&d, sizeof(char)))
 	{
-		fbin.read((char*)&d, sizeof(char));
 		cout << d;
 	}
-
-	cout << endl;
 	fbin.close();
 
 	return 0;
